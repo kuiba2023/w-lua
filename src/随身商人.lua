374b7a39e5321b7e9c279537d4dd19524706f75e
Your Name <you@example.com>
2023-07-12 21:34:18 +0800

    fix

--- src/随身商人.lua
+++ src/随身商人.lua
@@ -9,8 +9,8 @@ local OT=2
 local GOSSIP_ICON_CHAT            = 0                    -- 对话
 local GOSSIP_ICON_VENDOR          = 1                    -- 货物
 
---数据库
--- [[
+-- 数据库
+--[[
 REPLACE INTO `creature_template` (`entry`,`difficulty_entry_1`,`difficulty_entry_2`,`difficulty_entry_3`,`KillCredit1`,`KillCredit2`,`modelid1`,`modelid2`,`modelid3`,`modelid4`,`name`,`subname`,`IconName`,`gossip_menu_id`,`minlevel`,`maxlevel`,`exp`,`faction`,`npcflag`,`speed_walk`,`speed_run`,`speed_swim`,`speed_flight`,`detection_range`,`scale`,`rank`,`dmgschool`,`DamageModifier`,`BaseAttackTime`,`RangeAttackTime`,`BaseVariance`,`RangeVariance`,`unit_class`,`unit_flags`,`unit_flags2`,`dynamicflags`,`family`,`trainer_type`,`trainer_spell`,`trainer_class`,`trainer_race`,`type`,`type_flags`,`lootid`,`pickpocketloot`,`skinloot`,`PetSpellDataId`,`VehicleId`,`mingold`,`maxgold`,`AIName`,`MovementType`,`HoverHeight`,`HealthModifier`,`ManaModifier`,`ArmorModifier`,`ExperienceModifier`,`RacialLeader`,`movementId`,`RegenHealth`,`mechanic_immune_mask`,`spell_school_immune_mask`,`flags_extra`,`ScriptName`,`VerifiedBuild`) VALUES (]]..NPCID..[[,0,0,0,0,0,16104,0,0,0,']]..NPCNAME..[[',NULL,NULL,0,80,80,2,35,129,1,1.14286,1,1,20,1,0,0,1,2000,0,1,1,1,0,0,0,0,0,0,0,0,7,0,0,0,0,0,0,0,0,'',0,1,1,1,1,1,0,0,1,0,0,0,'',12340);
 ]]
 
